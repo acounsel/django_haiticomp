d13333361919399be307742d081413edb4f317b3
Samer <sarrabi@gmail.com>
2018-05-02 12:34:40 -0700

    fix forms ascii error

--- haiticomp/forms.py
+++ haiticomp/forms.py
@@ -9,7 +9,7 @@ class CompPackageForm(ModelForm):
         model = CompPackage
         fields = ('name', 'age', 'land_area', 'num_family')
         widgets = {
-            'name': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Sipòtè Destine'}),
+            'name': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Sipote Destine'}),
             'age': forms.TextInput(attrs={'class':'form-control', 'placeholder':'20'}),
             'num_family': forms.TextInput(attrs={'class':'form-control text-danger', 'placeholder':'6'}),
             'land_area': forms.TextInput(attrs={'class':'form-control text-primary', 'placeholder':'0.50'})
